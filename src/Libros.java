fe8f9efecdb6d7e7b865e1ed004d3e4eb224aa16
Manuvg <mvacag03@iesalbarregas.es>
2026-03-15 19:48:14 +0100

    Commit 2

--- src/Libros.java
+++ src/Libros.java
@@ -14,4 +14,28 @@ public class Libros {
         return "Título: " + nombre + ", Autor: " + autor;
     }
 
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getTitulo() {
+        return nombre;
+    }
+
+    public boolean isPrestado() {
+        return prestado;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrestado(boolean prestado) {
+        this.prestado = prestado;
+    }
+
 }
