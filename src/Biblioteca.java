2f1110dc41e21cf4f91196d9318b1674abc768e5
Manuvg <mvacag03@iesalbarregas.es>
2026-03-15 19:37:29 +0100

    Commit 3

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -8,4 +8,24 @@ public class Biblioteca {
         contador = 0;
     }
 
+    public void agregarLibro(Libros libro) {
+        if (contador < catalogo.length) {
+            catalogo[contador] = libro;
+            contador++;
+        } else {
+            System.out.println("La biblioteca está llena.");
+        }
+    }
+
+    public void mostrarLibros() {
+
+        System.out.println("📚 Catálogo de la Biblioteca:");
+        System.out.println("----------------------------");
+
+        System.out.println("Catálogo de la biblioteca:");
+        for (int i = 0; i < contador; i++) {
+            System.out.println((i + 1) + ". " + catalogo[i]);
+        }
+    }
+
 }
