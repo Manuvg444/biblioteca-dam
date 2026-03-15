403abc7d7981fe6b2b04aa5f31ae1834135e5e74
Manuvg <mvacag03@iesalbarregas.es>
2026-03-15 19:36:59 +0100

    Commit 2

--- src/App.java
+++ src/App.java
@@ -5,5 +5,10 @@ public class App {
 
         Libros libro1 = new Libros("Miguel Cervantes", "Quijote");
         Libros libro2 = new Libros("Cien Años de Soledad", "Gabriel García");
+
+        biblioteca.agregarLibro(libro1);
+        biblioteca.agregarLibro(libro2);
+
+        biblioteca.mostrarLibros();
     }
 }
